29646153290408f0588ac0621378123c411e5011
RunD1ck <rand1kch@gmail.com>
2019-11-29 11:32:41 +0300

    ПОшел назой

--- example/src/main/java/com/jsibbold/zoomage/example/MainActivity.java
+++ example/src/main/java/com/jsibbold/zoomage/example/MainActivity.java
@@ -17,24 +17,26 @@ package com.jsibbold.zoomage.example;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
-
 import android.util.Log;
-import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-import com.jsibbold.zoomage.AutoResetMode;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.jsibbold.zoomage.ZoomageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
